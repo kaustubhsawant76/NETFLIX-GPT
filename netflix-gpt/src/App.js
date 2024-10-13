be154c7796a2fb89a91a2f842bfa64dc35b36bdf
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-10-13 19:31:00 +0530

    cra and tailwind install

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-4xl text-green-400">
+     
+      MY NAME IS KAUSTUBH SAWANT
+       
+    </div>
+  );
+}
+
+export default App;
