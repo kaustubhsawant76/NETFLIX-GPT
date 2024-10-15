23e573ac4e1704fe2dd81eca5f65bda780c9006f
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-10-15 18:16:54 +0530

    login page

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -3,8 +3,12 @@ import Header from './Header'
 
 const Login = () => {
   return (
-    <div><Header/>
+    <div>
+        <div>
+    <Header/>
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f272782d-cf96-4988-a675-6db2afd165e0/web/IN-en-20241008-TRIFECTA-perspective_b28b640f-cee0-426b-ac3a-7c000d3b41b7_large.jpg" alt="logo"  />
+        </div>
+        
     </div>
   )
 }
