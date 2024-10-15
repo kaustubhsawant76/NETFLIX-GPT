b6b597f5c95ca024f1de3f376de6fbe9447afb0d
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-10-15 16:19:18 +0530

    login page

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,14 +1,12 @@
+import React from 'react'
+import Body from './components/Body'
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App text-4xl text-green-400">
-     
-      MY NAME IS KAUSTUBH SAWANT
-       
-    </div>
-  );
+    <>
+    <Body/>
+    </>
+  )
 }
 
-export default App;
+export default App
